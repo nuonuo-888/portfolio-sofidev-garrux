790c1ffc764919107e29b2e051c9a7e8e4b9ebc0
nuonuo-888 <2720161028@qq.com>
2025-11-25 19:21:54 +0800

    chore: upgrade astro to v5

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 import sitemap from "@astrojs/sitemap";
+import edgeoneAdapter from '@edgeone/astro';
 
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
     enabled: false
   },
+  adapter: edgeoneAdapter(),
   integrations: [react(), tailwind(), sitemap()]
 });
